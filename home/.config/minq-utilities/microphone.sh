fd5071deaa18a03a71ba91e349cc27f211bebd80
me <znamsiparolata4esno@abv.bg>
2022-10-04 12:10:23 +0300

    update

--- home/.config/minq-utilities/microphone.sh
+++ home/.config/minq-utilities/microphone.sh
@@ -1,3 +1,9 @@
 #! /usr/bin/env bash
 
-awk -F"[][]" '/Mono:/ { print $4 }' <(amixer set Capture toggle)
+# TODO make this work if mic is `front left` and `front right` instead of `mono`
+
+# this works for `mono`
+#awk -F"[][]" '/Mono:/ { print $4 }' <(amixer set Capture toggle)
+
+# this works for `front right`
+amixer set Capture toggle | tail -1 | awk -F"[][]" '{print $4}'
