c1a2a2af2a6e933271669007cebb6e5f5979e53a
me <znamsiparolata4esno@abv.bg>
2022-10-10 10:14:25 +0300

    update

--- home/.config/minq-utilities/microphone-get-pango.sh
+++ home/.config/minq-utilities/microphone-get-pango.sh
@@ -1,6 +1,6 @@
 #! /usr/bin/env bash
 
-# TODO this doesn't work on the very first boot
+here=$(dirname "$0")
 
 # this only works for `mono`
 #awk -F"[][]" '/Mono:/ { print $4 }' <(amixer set Capture toggle)
@@ -9,6 +9,7 @@ all_output=$(amixer get Capture toggle)
 last_line=$(tail -1 <<< "${all_output}")
 on_or_off=$(awk -F"[][]" '{print $4}' <<< "${last_line}")
 
+# this obly works with `pango` markdown
 case "${on_or_off}" in
 	"on")
 		fg_col='green'
@@ -22,5 +23,10 @@ case "${on_or_off}" in
 		echo "${all_output}" > /tmp/microphone-widget-error-log
 		;;
 esac
+
+# this only works if the icon was clicked in `i3blocks`
+if [ "${button}" != "" ]; then
+	"${here}"/microphone-set.sh > /dev/null
+fi
 
 echo "<span foreground=\"${fg_col}\">${on_or_off}</span>"
