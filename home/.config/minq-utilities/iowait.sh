8553cd54596eaaebbf8fdba3a16ef5726bfc4d0d
me <znamsiparolata4esno@abv.bg>
2022-10-18 21:23:50 +0300

    update

--- home/.config/minq-utilities/iowait.sh
+++ home/.config/minq-utilities/iowait.sh
@@ -4,7 +4,9 @@
 
 # mpstat | tail -1 | awk '{print $7}'
 
+set -e
+
 requested_time="$1"
 
-mpstat --dec=0 "$requested_time" 1 | tail -1 | awk '{print $6}'
+mpstat --dec=0 "${requested_time}" 1 | tail -1 | awk '{print $6}'
 	# `--dec` stands for number of decimal places. default is `2`
