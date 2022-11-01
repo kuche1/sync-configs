dabd8e4ce17a77b9653fceaea6e15c9059ae484f
me <znamsiparolata4esno@abv.bg>
2022-11-01 18:40:07 +0200

    update

--- home/.config/minq-utilities/date-and-time.sh
+++ home/.config/minq-utilities/date-and-time.sh
@@ -4,5 +4,5 @@ if [ "${button}" != "" ]; then
     dde-calendar > /dev/null &
 fi
 
-date '+%Y-%m-%d w%V %H:%M:%S'
+date '+%Y-%m-%d w%V:%u/7 %H:%M:%S'
 # if u want sunday as first day of the week u can use `%U` instead of `%V`
