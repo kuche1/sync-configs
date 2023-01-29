152b70f3137030ddaa0f2b8f41f114aa3c216c86
me <znamsiparolata4esno@abv.bg>
2023-01-29 14:37:46 +0200

    update

--- home/.config/minq-utilities/dangerous/arch/arch-install-lvm.sh
+++ home/.config/minq-utilities/dangerous/arch/arch-install-lvm.sh
@@ -18,10 +18,11 @@ pkg_install(){
 aur_install(){
 	# chroot_run su me -c "echo \"${user_password}\" | paru --sudo sudo --sudoflags -S --noconfirm -S --needed \"$@\""
 	(cat << EOF
+su me
 echo "#! /usr/bin/env bash" > /tmp/free-sudo.sh
 echo "\"${user_password}\" | sudo -S -k \"\$@\"" >> /tmp/free-sudo.sh
-chown me /tmp/free-sudo.sh
-su me -c "paru --sudo /tmp/free-sudo.sh --noconfirm -S --needed \"$@\""
+paru --sudo /tmp/free-sudo.sh --noconfirm -S --needed "$@"
+exit
 EOF
 	) | chroot_run
 	# TODO chown might have been the problem with the `visudo` fail
