ae095edd8274338a4c47bbaf2fe9185b86997229
kuche1 <znamsiparolata4esno@abv.bg>
2023-01-31 17:48:39 +0200

    update

--- home/.config/minq-utilities/microphone-get-pango.sh
+++ home/.config/minq-utilities/microphone-get-pango.sh
@@ -5,9 +5,24 @@ here=$(dirname "$BASH_SOURCE")
 # this only works for `mono`
 #awk -F"[][]" '/Mono:/ { print $4 }' <(amixer set Capture toggle)
 
-all_output=$(amixer get Capture toggle)
-last_line=$(tail -1 <<< "${all_output}")
-on_or_off=$(awk -F"[][]" '{print $4}' <<< "${last_line}")
+# wtf this stopped working
+# all_output=$(amixer get Capture toggle)
+# last_line=$(tail -1 <<< "${all_output}")
+# on_or_off=$(awk -F"[][]" '{print $4}' <<< "${last_line}")
+
+source=$(pactl info | grep "Default Source" | cut -f3 -d" ")
+mute_info=$(pactl list sources | grep -A 10 "${source}" | grep 'Mute: ' | cut -f2 -d' ')
+case "${mute_info}" in
+	'yes')
+		on_or_off='off'
+		;;
+	'no')
+		on_or_off='on'
+		;;
+	*)
+		on_or_off="unknown mute state: ${mute_info}"
+		;;
+esac
 
 # this only works with `pango` markdown
 case "${on_or_off}" in
@@ -19,7 +34,7 @@ case "${on_or_off}" in
 		;;
 	*)
 		fg_col='yellow'
-		on_or_off="ERROR"
+		on_or_off="ERROR: ${on_or_off}"
 		;;
 esac
 
--- home/.config/minq-utilities/microphone-set.sh
+++ home/.config/minq-utilities/microphone-set.sh
@@ -1,7 +1,6 @@
 #! /usr/bin/env bash
 
-amixer set Capture toggle
+#amixer set Capture toggle
+	# for some reason this didn't work on my latest installation
 
-# could also use
-# pactl set-source-mute @DEFAULT_SOURCE@ toggle
-# ?
+pactl set-source-mute @DEFAULT_SOURCE@ toggle
