6aa8f45df2d378c12ed32125a698e82493ba3c57
kuche1 <znamsiparolata4esno@abv.bg>
2023-02-25 02:06:12 +0200

    update

--- home/.config/minq-utilities/dangerous/arch-install-lvm.sh
+++ home/.config/minq-utilities/dangerous/arch-install-lvm.sh
@@ -521,7 +521,7 @@ pkg_install tumbler # thumbnails
 	pkg_install libopenraw # raw
 	pkg_install freetype2 # font
 #caja caja-open-terminal
-arch_chroot xdg-mime default thunar.desktop inode/directory
+chroot_run xdg-mime default thunar.desktop inode/directory
 	# set as default file browser
 	# TODO maybe this needs to be executed as the user
 
