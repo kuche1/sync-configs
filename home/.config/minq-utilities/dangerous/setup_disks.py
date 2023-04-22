0f7745a0dbe7242c0cdded16705806c97aed4677
kuche1 <znamsiparolata4esno@abv.bg>
2023-04-22 22:26:35 +0300

    update

--- home/.config/minq-utilities/dangerous/setup_disks.py
+++ home/.config/minq-utilities/dangerous/setup_disks.py
@@ -6,8 +6,6 @@ import json
 VOL_GR_NAME = 'myVolGr'
 LV_NAME = 'myRootVol'
 
-SECTOR_SIZE = 512 # TODO this might be wrong
-
 def term(cmd:list[str], *a, **kw):
     print(f'+ running: {" ".join(cmd)}')
     return subprocess.run(cmd, check=True, *a, **kw)
@@ -16,20 +14,23 @@ def term_out(cmd:list[str]):
     return term(cmd, capture_output=True).stdout.decode()
 
 class Device:
-    def __init__(s, path, cur_part):
+    def __init__(s, path, cur_part, taken_space):
         s.path = path
         s.part = cur_part
         s.used_space = 0
-        s.total_space = int(term_out(['blockdev', '--getsize64', path]))
-        s.free_space = s.total_space - s.used_space
-    def __lt__(s, other):
-        return s.free_space < other.free_space
-    def use_space(s, size):
-        remainder = size % SECTOR_SIZE
-        if remainder != 0:
-            size += (SECTOR_SIZE - remainder)
-        s.used_space += size
-        s.free_space -= size
+        s.taken_space = taken_space # in GiB
+    # def __lt__(s, other):
+    #     return s.free_space < other.free_space
+    # def use_space(s, size):
+    #     s.used_space += size
+    #     s.free_space -= size
+    def get_cur_space(s)->str:
+        if s.taken_space == 0:
+            return '0%'
+        return f'{s.taken_space}GiB'
+    def inc_cur_space(s, size:int)->str:
+        s.taken_space += size
+        return f'{s.taken_space}GiB'
 
 out = term_out(['lsblk', '--json'])
 devices = json.loads(out)
@@ -51,69 +52,63 @@ while True:
     devices.remove(dev)
     selected_devices.append(dev)
 
-devices = selected_devices
+devs = selected_devices
 assert len(devices) > 0
 
-print(f'Select boot device: {devices}')
+for dev in devs:
+    term(['parted', '-s', dev, 'mklabel', 'gpt'])
+
+print(f'Select boot device: {devs}')
 dev = input('> ')
 
 boot_dev = dev
-devices.remove(boot_dev)
+devs.remove(boot_dev)
 
-term(['parted', '-s', boot_dev, 'mklabel', 'gpt'])
-boot_dev_bytes = 1024 * 1024 * 512 # 512 MiB
-term(['parted', '-s', boot_dev, 'mkpart', 'primary', 'fat32', '0%', f'{boot_dev_bytes}B'])
+term(['parted', '-s', boot_dev, 'mkpart', 'primary', 'fat32', '0%', '1GiB'])
 term(['parted', '-s', boot_dev, 'set', '1', 'esp', 'on'])
-
 term(['mkfs.fat', '-F32', f'{boot_dev}1'])
 
-devs = [Device(boot_dev, 2)]
-devs[0].use_space(boot_dev_bytes)
-for dev in devices:
-    term(['parted', '-s', dev, 'mklabel', 'gpt'])
-    devs.append(Device(dev, 1))
+devs = [Device(dev, 0, 0) for dev in devs] + [Device(boot_dev, 1, 1)]
 
 cur_md = 0
 
 while True:
     if len(devs) <= 1:
         break
-    
-    smallest = min(devs)
-    size = smallest.free_space
 
+    print(term_out(['lsblk']))
+    size = input('Select size for next raid0 (example: 80 (in GiB)): ')
+    assert int(size) == float(size) # needs to be an int
+    size = int(size)
     for dev in devs:
-        #term(['parted', '-s', dev.path, 'mkpart', 'primary', 'ext4', f'{dev.used_space}B', f'{dev.used_space+size}B'])
-        term(['parted', '-s', dev.path, 'mkpart', 'primary', f'{dev.used_space+1}B', f'{dev.used_space+size}B'])
+        term(['parted', '-s', dev.path, 'mkpart', 'primary', dev.get_cur_space(), dev.inc_cur_space(size)])
+        dev.part += 1
         term(['parted', '-s', dev.path, 'set', dev.part, 'raid', 'on'])
-        dev.use_space(size)
 
-    # for dev in devs:
-    #     term(['mkfs.ext4', '-F', f'{dev.path}{dev.part}'])
-
-    term(['mdadm', '-Cv', '-l0', '-c64', f'-n{len(devs)}', f'/dev/md{cur_md}'] + [f'{dev.path}{dev.part}' for dev in devs])
+    term(['mdadm', '-Cv', '-l0', '-c64', f'-n{len(devs)}', f'/dev/md{cur_md}'] + [dev.path for dev in devs])
     cur_md += 1
 
-    for dev in devs:
-        dev.part += 1
+    while True:
+        print(term_out(['lsblk']))
+        paths = [dev.path for dev in devs]
+        print(f'Select which device is now full (leave empty to stop): {paths}')
+        dev = input('> ')
+        if dev == '':
+            break
+        paths.remove(dev)
+        del devs[paths.index(dev)]
 
-    devs.remove(smallest)
+mds = []
+for md in range(cur_md):
+    md = f'/dev/md{md}'
+    mds.append(md)
+    # term(['mkfs.ext4', md])
 
-    to_delete = []
-    for dev in devs:
-        if dev.free_space == 0:
-            to_delete.append(dev)
-    
-    for dev in to_delete:
-        devs.remove(dev)
+for md in mds:
+    term(['pvcreate', md])
 
-parts = [f'/dev/md{x}' for x in range(cur_md)] + devs
-for part in parts:
-    #term(['mkfs.ext4', '-F', f'{part}'])
-    term(['pvcreate', part])
+term(['vgcreate', 'myVolGr'] + mds + devs) # include all raid0s and the empty space of the last disk (if there is one)
 
-term(['vgcreate', VOL_GR_NAME] + parts)
+term(['lvcreate', '--yes', '-l', '100%FREE', 'myVolGr', '-n', 'myRootVol'])
 
-term(['lvcreate', '--yes', '-l', '100%FREE', VOL_GR_NAME, '-n', LV_NAME])
-
-term(['mkfs.ext4', '-F', f'/dev/mapper/{VOL_GR_NAME}-{LV_NAME}'])
+term(['mkfs.ext4', '-F', '/dev/mapper/myVolGr-myRootVol'])
