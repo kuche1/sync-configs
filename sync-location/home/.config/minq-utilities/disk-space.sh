81601ec565a50b0211c3ef21aa51ef76881a9b2e
kuche1 <znamsiparolata4esno@abv.bg>
2023-09-23 22:51:13 +0300

    update

--- sync-location/home/.config/minq-utilities/disk-space.sh
+++ sync-location/home/.config/minq-utilities/disk-space.sh
@@ -1,4 +1,4 @@
 #! /usr/bin/env bash
 
 free_space=$(df -h / | tail -1 | awk '{print $4}')
-echo "Porn folder: $free_space"
+echo "porn folder: $free_space"
--- sync-location/home/.config/minq-utilities/volume-get.sh
+++ sync-location/home/.config/minq-utilities/volume-get.sh
@@ -4,7 +4,8 @@ set -e
 
 #awk -F"[][]" '/Left:/ { print $2 }' <(amixer sget Master)
 vol=$(awk -F"[][]" '/Left:/ { print $2 }' <(amixer -D pulse sget Master))
+
 vol="${vol::-1}"
-	# delete the last character - `%`
+# delete the last character - `%`
 
 echo "${vol}"
