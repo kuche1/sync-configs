09a35daf213254e35de850d4ab6a68c033211259
kuche1 <znamsiparolata4esno@abv.bg>
2023-11-04 21:55:31 +0200

    update

--- sync-location/home/.config/minq-utilities/disk-space.sh
+++ sync-location/home/.config/minq-utilities/disk-space.sh
@@ -1,4 +1,5 @@
 #! /usr/bin/env bash
 
 free_space=$(df -h / | tail -1 | awk '{print $4}')
+free_space=$(echo "$free_space" | python3 -c 'print(input().replace(",", "."))')
 echo "porn folder: $free_space"
