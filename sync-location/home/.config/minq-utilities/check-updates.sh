f9fe1e2200696bcd04f0cbaff0e352702197c5b0
me <znamsiparolata4esno@abv.bg>
2023-12-26 15:33:26 +0200

    update

--- sync-location/home/.config/minq-utilities/check-updates.sh
+++ sync-location/home/.config/minq-utilities/check-updates.sh
@@ -3,7 +3,7 @@
 regular_updates=$(
 	(
 		(checkupdates --version >/dev/null && checkupdates) ;
-		(apt --version >/dev/null          && apt list --upgradable) ;
+		(apt --version >/dev/null          && apt list --upgradable) ; # TODO this needs to be reduces by 1
 	) | wc -l
 )
 
