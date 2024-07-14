04e77633b434648cfabdcc7c50954cf27dc591a5
kuche1 <znamsiparolata4esno@abv.bg>
2024-07-14 17:43:26 +0300

    update

--- sync-location/home/.config/minq-utilities/disk-space.sh
+++ sync-location/home/.config/minq-utilities/disk-space.sh
@@ -1,5 +1,13 @@
 #! /usr/bin/env bash
 
-free_space=$(df -h / | tail -1 | awk '{print $4}')
-free_space=$(echo "$free_space" | python3 -c 'print(input().replace(",", "."))')
-echo "porn folder: $free_space"
+### doesn't quite work on ZFS
+#
+# free_space=$(df -h / | tail -1 | awk '{print $4}')
+# free_space=$(echo "$free_space" | python3 -c 'print(input().replace(",", "."))')
+# #echo "porn folder: $free_space"
+# echo "$free_space"
+
+free=$(zpool list | tail -1 | awk '{print $4}')
+total=$(zpool list | tail -1 | awk '{print $2}')
+
+echo "$free/$total"
